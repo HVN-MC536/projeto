2eed2f4cf435759bb321f4dbdad3dcc943a65b5f
natan <Natan Rodrigues de Oliveira>
2020-11-25 12:43:17 -0300

    atualização da normalização

---
+++ stage03/normalizacao.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+tabela = pd.read_csv('tabela.csv')
+
+# print(tabela[['age','generation']])
+
+def normalize_generation(tabela):
+    age_gen = tabela[['age', 'generation']]
+
+    gen_arr = np.unique(age_gen['generation'].values)
+    age_arr = np.unique(age_gen['age'].values)
+
+    gen_table = {'G.I. Generation' : '75+ years', 'Silent' : '55-74 years',
+                'Boomers' : '35-54 years', 'Generation X' : '25-34 years',
+                'Millenials' : '15-24 years', 'Generation Z' : '5-14 years'}
+            
+    gen_final_table = pd.DataFrame({'generation' : gen_table.keys(), 'age' : gen_table.values()})
+    gen_final_table.to_csv('generation.csv', index=False)
+
+def normalize_pais(tabela):
+    pais = tabela[['country', 'year', 'sex', 'population']]
+    # print(pais)
+    res = pais.groupby(by=['country', 'year', 'sex']).sum()
+    ressize = len(res.index)
+    population = np.array(int(ressize/2)*[0])
+    vec = res.values
+    # print(vec)
+    for i in range(0,ressize,2):
+        population[int(i/2)] = vec[i][0]+vec[i+1][0]
+
+    pais = pais[['country', 'year']]
+    pais_vec = np.unique(pais['country'].values)
+    year_vec = np.unique(pais['year'].values)
+    # print(pais_vec)
+    # print(year_vec)
+
+    pop = 0
+    pais_table = [[], [], []]
+    for p in pais_vec:
+        for y in year_vec:
+            if pais.eq([p, y]).all(1).any():
+                pais_table[0].append(p)
+                pais_table[1].append(y)
+                pais_table[2].append(population[pop])
+                pop += 1
+    # print(count)
+    # print(len(population))
+    # print(pais_table)
+
+    final_table = pd.DataFrame({'country' : pais_table[0], 'year' : pais_table[1], 'population' : pais_table[2]})
+    final_table.to_csv('pais.csv', index=False)
