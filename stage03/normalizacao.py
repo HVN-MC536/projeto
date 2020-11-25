7b7a60c9b34a8b96c2362ae0732abfb33d0bd25e
natan <Natan Rodrigues de Oliveira>
2020-11-25 15:31:36 -0300

    finalização da normalização

--- stage03/normalizacao.py
+++ stage03/normalizacao.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import numpy as np
-tabela = pd.read_csv('tabela.csv')
-
-# print(tabela[['age','generation']])
 
 def normalize_generation(tabela):
     age_gen = tabela[['age', 'generation']]
@@ -19,20 +16,17 @@ def normalize_generation(tabela):
 
 def normalize_pais(tabela):
     pais = tabela[['country', 'year', 'sex', 'population']]
-    # print(pais)
     res = pais.groupby(by=['country', 'year', 'sex']).sum()
     ressize = len(res.index)
     population = np.array(int(ressize/2)*[0])
     vec = res.values
-    # print(vec)
+
     for i in range(0,ressize,2):
         population[int(i/2)] = vec[i][0]+vec[i+1][0]
 
     pais = pais[['country', 'year']]
     pais_vec = np.unique(pais['country'].values)
     year_vec = np.unique(pais['year'].values)
-    # print(pais_vec)
-    # print(year_vec)
 
     pop = 0
     pais_table = [[], [], []]
@@ -43,9 +37,19 @@ def normalize_pais(tabela):
                 pais_table[1].append(y)
                 pais_table[2].append(population[pop])
                 pop += 1
-    # print(count)
-    # print(len(population))
-    # print(pais_table)
 
     final_table = pd.DataFrame({'country' : pais_table[0], 'year' : pais_table[1], 'population' : pais_table[2]})
     final_table.to_csv('pais.csv', index=False)
+
+def normalize_principal(tabela):
+    principal = tabela[['country', 'year', 'sex', 'age', 'suicides_no', 'suicides_rate']]
+    principal.to_csv('suicides.csv', index=False)
+
+def main():
+    tabela = pd.read_csv('tabela.csv')
+    normalize_generation(tabela)
+    normalize_pais(tabela)
+    normalize_principal(tabela)
+
+if __name__ == "__main__":
+    main()
