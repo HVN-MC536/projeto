4242c85d7c487a28c6fbf74da4d0dbad6cb65f45
natan <Natan Rodrigues de Oliveira>
2020-11-26 08:12:50 -0300

    a tabela pais não tinha os gdps, incluí agora

--- stage03/normalizacao.py
+++ stage03/normalizacao.py
@@ -15,8 +15,12 @@ def normalize_generation(tabela):
     gen_final_table.to_csv('generation.csv', index=False)
 
 def normalize_pais(tabela):
-    pais = tabela[['country', 'year', 'sex', 'population']]
-    res = pais.groupby(by=['country', 'year', 'sex']).sum()
+    pais_pop = tabela[['country', 'year', 'sex', 'population']]
+    pais_gdp = tabela[['country', 'year', 'gdp_for_year', 'gdp_per_capita']]
+
+    gdp = pais_gdp.groupby(['country', 'year']).max()
+
+    res = pais_pop.groupby(by=['country', 'year', 'sex']).sum()
     ressize = len(res.index)
     population = np.array(int(ressize/2)*[0])
     vec = res.values
@@ -24,21 +28,26 @@ def normalize_pais(tabela):
     for i in range(0,ressize,2):
         population[int(i/2)] = vec[i][0]+vec[i+1][0]
 
-    pais = pais[['country', 'year']]
-    pais_vec = np.unique(pais['country'].values)
-    year_vec = np.unique(pais['year'].values)
+    pais_pop = pais_pop[['country', 'year']]
+    pais_vec = np.unique(pais_pop['country'].values)
+    year_vec = np.unique(pais_pop['year'].values)
 
     pop = 0
-    pais_table = [[], [], []]
+    pais_table = [[], [], [], [], []]
     for p in pais_vec:
         for y in year_vec:
-            if pais.eq([p, y]).all(1).any():
+            if pais_pop.eq([p, y]).all(1).any():
                 pais_table[0].append(p)
                 pais_table[1].append(y)
                 pais_table[2].append(population[pop])
                 pop += 1
+    for i in range(len(gdp.index)):
+        pais_table[3].append(gdp['gdp_for_year'][i])
+        pais_table[4].append(gdp['gdp_per_capita'][i])
 
-    final_table = pd.DataFrame({'country' : pais_table[0], 'year' : pais_table[1], 'population' : pais_table[2]})
+    final_table = pd.DataFrame({'country' : pais_table[0], 'year' : pais_table[1],
+                                'population' : pais_table[2], 'gdp_for_year' : pais_table[3],
+                                'gdp_per_capita' : pais_table[4]})
     final_table.to_csv('pais.csv', index=False)
 
 def normalize_principal(tabela):
@@ -47,9 +56,9 @@ def normalize_principal(tabela):
 
 def main():
     tabela = pd.read_csv('tabela.csv')
-    normalize_generation(tabela)
+    # normalize_generation(tabela)
     normalize_pais(tabela)
-    normalize_principal(tabela)
+    # normalize_principal(tabela)
 
 if __name__ == "__main__":
     main()
